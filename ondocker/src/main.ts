f87f5b400aacbcf4cb6c96ff03dc59bb39f46e39
tulparstudyo <teknovipyazilim@gmail.com>
2022-06-14 19:08:29 +0300

    Update main.ts

--- ondocker/src/main.ts
+++ ondocker/src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.connectMicroservice({
     transport: Transport.TCP,
-    options: { host:'Micro', port: 3102 },
+    options: { host:'172.17.0.3', port: 3102 },
   });
   await app.startAllMicroservices();
   //await app.listen();
