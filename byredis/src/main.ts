45f06f9b77a697d236910bd6c14034ab9f5a5a2b
tulparstudyo <teknovipyazilim@gmail.com>
2022-06-14 17:26:47 +0300

    redis

--- byredis/src/main.ts
+++ byredis/src/main.ts
@@ -3,13 +3,14 @@ import { Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+    const url = 'redis://localhost:6379';
     const app = await NestFactory.createMicroservice(AppModule, {
         transport: Transport.REDIS,
         options: {
-            url: 'redis://localhost:6379',
+            url: url,
         },
     });
-    console.log('MoviesService is running on 6379');
+    console.log('MoviesService is running on ' + url );
     await app.listen();
 }
 bootstrap();
